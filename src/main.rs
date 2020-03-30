25ef27adb7279eb885bebfee10eeb0079df202c3
Maxim Vinogradov <maxvinza@yandex.ru>
2020-03-30 17:42:58 +0200

    fix and add readme

--- src/main.rs
+++ src/main.rs
@@ -12,35 +12,32 @@ fn main() {
             println!("\nUsage ./bin_to_const [FILENAME] > out.txt\n");
         } else {
             let mut file = File::open(&arguments_collection[1]).unwrap();
-            let mut buffer = [0; 10];
+            let mut buffer = Vec::new();
             println!("\nconst DATA: &[u8] = &[");
             let mut counter = 0;
             let mut printed_string = String::new();
-            while file.read(&mut buffer).unwrap() > 0 {
-                for byte in &buffer {
-                    if counter > 0 {
-                        printed_string.push_str(", 0x");
-                    } else {
-                        printed_string.push_str("    0x");
-                    }
-
-                    let text_byte = format!("{:x}", byte);
-
-                    if text_byte.len() == 1 {
-                        printed_string.push('0');
-                    }
-
-                    printed_string.push_str(&text_byte);
-                    counter += 1;
-
-                    if counter > 10 {
-                        println!("{},", printed_string);
-                        counter = 0;
-                        printed_string = String::new();
-                    }
+            file.read_to_end(&mut buffer).unwrap();
+            for byte in &buffer {
+                if counter > 0 {
+                    printed_string.push_str(", 0x");
+                } else {
+                    printed_string.push_str("    0x");
                 }
 
-                buffer = [0; 10];
+                let text_byte = format!("{:x}", byte);
+
+                if text_byte.len() == 1 {
+                    printed_string.push('0');
+                }
+
+                printed_string.push_str(&text_byte);
+                counter += 1;
+
+                if counter > 10 {
+                    println!("{},", printed_string);
+                    counter = 0;
+                    printed_string = String::new();
+                }
             }
 
             if printed_string.len() > 0 {
